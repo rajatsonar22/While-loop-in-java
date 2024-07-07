b6a1db941bf3226b5ee41ab240c49ea006f2b9b4
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2024-07-07 09:10:57 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,11 @@
+public class Whileloop{
+    public static void main(String args[]){
+        // While loop in program 
+        int i = 0;
+        System.out.println("Printing 1-10 using While loop");
+        while(i < 11){
+            System.out.println(i);
+            i = i+ 1;
+        }
+    }
+}
